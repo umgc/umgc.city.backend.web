4abb3e374b0eedeea6bda554bc117d699486278d
Krystina Poling <krystina627@gmail.com>
2020-04-12 17:28:17 -0400

    Updated DDL script

--- src/main/java/umgc/city/team1/repositories/AuthoritiesRepository.java
+++ src/main/java/umgc/city/team1/repositories/AuthoritiesRepository.java
@@ -1,8 +1,9 @@
 package umgc.city.team1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import umgc.city.team1.models.Authorities;
 
 import java.util.UUID;
 
-public interface Authorities extends JpaRepository<Application, UUID> {
+public interface AuthoritiesRepository extends JpaRepository<Authorities, UUID> {
 }
